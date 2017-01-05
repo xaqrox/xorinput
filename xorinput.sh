7ba2378698aa0b941832b30f1bda28bbf23ff0ff
Xaq Rothman <xaq@radcampaign.com>
2017-01-05 16:54:44 -0500

    A little scripty, very bad idea.

--- xorinput.sh
+++ xorinput.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 # Grep xinput device list for laptop keyboard.
+# Mine is "AT Translated Set 2 keyboard"
 # @TODO make this configurable
-device_info=`xinput list | grep 'AT'`
+device_info=`xinput list | grep 'AT'`;
 
 # Parse xinput line for slave and master ids.
-slave_id=`echo $device_info | sed 's/.* id=\([0-9]*\) .*/\1/'`
-master_id=`echo $device_info | sed 's/.*slave keyboard (\([0-9]*\)).*/\1/'`
+slave_id=`echo $device_info | sed 's/.* id=\([0-9]*\) .*/\1/'`;
+master_id=`echo $device_info | sed 's/.*slave keyboard (\([0-9]*\)).*/\1/'`;
 
 # Disable laptop keyboard.
-xinput float $slave_id
+xinput float $slave_id;
+echo "laptop keyboard disabled";
+echo "run 'xinput reattach $slave_id $master_id' in case of emergency";
 
 # Wait for input.
-read -p "press any key to renable laptop keyboard" key
+read -p "press any key to renable laptop keyboard" key;
 
 # Re-enable laptop keyboard.
-xinput reattach $slave_id $master_id
+xinput reattach $slave_id $master_id;
